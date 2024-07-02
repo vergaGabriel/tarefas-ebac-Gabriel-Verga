b0e63563f193cd3cad88e3f5e66c79b7edb97cb2
vergaGabriel <gabriel_verga@hotmail.com>
2024-07-02 09:34:57 -0300

    Adicionando a tarefa do Modulo 12

---
+++ Modulo_12/tarefa_colecoes_parte_2/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        List<String> listaHomens = new ArrayList<String>();
+        List<String> listaMulheres = new ArrayList<String>();
+
+        System.out.println("Informe o nome e o sexo (M ou F) das pessoas (separando o sexo por '-' e separando os nomes por ','): ");
+
+        String pessoas = scanner.nextLine();
+        String[] pessoasSplit = pessoas.split(",");
+
+        for(String pessoa : pessoasSplit){
+            String[] info = pessoa.split("-");
+            if(info[1].equalsIgnoreCase("M")){
+                listaHomens.add(info[0]);
+            } else {
+                listaMulheres.add(info[0]);
+            }
+        }
+
+        System.out.println("Grupo com as pessoas do sexo masculino: " + listaHomens);
+        System.out.println("Grupo com as pessoas do sexo feminino: " + listaMulheres);
+    }
+}
