926f400908425ae0ebfe9229d55906675a3e91eb
vergaGabriel <gabriel_verga@hotmail.com>
2024-11-18 21:03:17 -0300

    Adicionando classe "Produto"

---
+++ Modulo32/ExemplosMod32/src/main/java/br/com/gverga/dao/IProdutoDao.java
@@ -0,0 +1,8 @@
+package br.com.gverga.dao;
+
+import br.com.gverga.domain.Produto;
+
+public interface IProdutoDao {
+
+    public Produto cadastrar(Produto produto);
+}
---
+++ Modulo32/ExemplosMod32/src/test/java/br/com/gverga/ProdutoTest.java
@@ -0,0 +1,28 @@
+package br.com.gverga;
+
+import br.com.gverga.dao.IProdutoDao;
+import br.com.gverga.dao.ProdutoDao;
+import br.com.gverga.domain.Produto;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProdutoTest {
+    private IProdutoDao produtoDao;
+
+    public ProdutoTest() {
+        produtoDao = new ProdutoDao();
+    }
+
+    @Test
+    public void cadastrar() {
+        Produto produto = new Produto();
+        produto.setCodigo("A1");
+        produto.setPreco(200D);
+        produto.setNome("Curso de Java Backend");
+        produto = produtoDao.cadastrar(produto);
+
+        Assert.assertNotNull(produto);
+        Assert.assertNotNull(produto.getId());
+    }
+}
